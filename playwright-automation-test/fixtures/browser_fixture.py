6128b77fb3efe9edb0c7bf5c3210d7ba9da0068a
MilesShi <bridgeshi93@gmail.com>
2025-12-14 17:19:33 +0800

    fix: Update browser context creation and Docker setup for Playwright

--- playwright-automation-test/fixtures/browser_fixture.py
+++ playwright-automation-test/fixtures/browser_fixture.py
@@ -2,6 +2,7 @@ import pytest
 import logging
 
 from playwright.async_api import ViewportSize
+from playwright.sync_api import ViewportSize
 from playwright.sync_api import sync_playwright
 from fixtures.report_fixture import save_screenshot
 
@@ -13,14 +14,14 @@ def create_context(browser, test_directory, locale="en-US"):
     Create a new browser context with specified configurations.
 
     :param browser: The Playwright browser instance used to create a new context.
-    :type browser: playwright.async_api.Browser
+    :type browser: playwright.sync_api.Browser
     :param test_directory: The base directory where test-related files, including videos, will be stored.
     :type test_directory: pathlib.Path
     :param locale: The locale to use in the context (e.g., 'en-US'). Defaults to 'en-US'.
     :type locale: str
 
     :returns: A new browser context configured with the provided settings.
-    :rtype: playwright.async_api.BrowserContext
+    :rtype: playwright.sync_api.BrowserContext
     """
     video_dir = test_directory / "videos"
     video_dir.mkdir(parents=True, exist_ok=True)
@@ -28,7 +29,7 @@ def create_context(browser, test_directory, locale="en-US"):
         ignore_https_errors=True,
         accept_downloads=True,
         record_video_dir=str(video_dir),
-        viewport=ViewportSize(width=1920, height=1080),
+        viewport=ViewportSize({"width": 1920, "height": 1080}),
         locale=locale,
     )
     return context
@@ -81,7 +82,6 @@ def page(browser, test_directory, request):
     :param test_directory: 测试结果目录 - 来自于 report_fixture.py
     :param request: pytest 请求对象
     """
-    # page = browser.new_page()
     context = create_context(browser, test_directory)
     page = context.new_page()
 
@@ -95,4 +95,6 @@ def page(browser, test_directory, request):
 
     # 2️⃣ 关闭页面和上下文
     logger.info("close the page")
+    context.close()
     page.close()
+
